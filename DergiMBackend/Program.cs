3bcb3dc2cdbed103b829bd730c3ba641c944c255
OleksanderPylypchuk <sashylichka2016@gmail.com>
2025-04-07 10:40:03 +0300

    DbContext, Users and mapper setup

--- DergiMBackend/Program.cs
+++ DergiMBackend/Program.cs
@@ -1,11 +1,27 @@
+using AutoMapper;
+using DergiMBackend;
+using DergiMBackend.DbContext;
+using DergiMBackend.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
+});
 
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+IMapper mapper = MapperConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
