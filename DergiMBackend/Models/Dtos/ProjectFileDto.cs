c7fe54eeb3190b62cc7d540d401a2cf8feea196f
OlexanderPylypchuk <sashylichka2016@gmail.com>
2025-04-14 13:08:57 +0300

    ProjectFile, Dto and Mapping

---
+++ DergiMBackend/Models/Dtos/ProjectFileDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace DergiMBackend.Models.Dtos
+{
+	public class ProjectFileDto
+	{
+		public int Id { get; set; }
+		public string FileUrl { get; set; }
+		public int ProjectId { get; set; }
+	}
+}
---
+++ DergiMBackend/Models/ProjectFile.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DergiMBackend.Models
+{
+	public class ProjectFile
+	{
+		public int Id { get; set; }
+		public string FileUrl { get; set; }
+		public string LocalFileUrl { get; set; }
+		[ForeignKey(nameof(Project))]
+		public int ProjectId { get; set; }
+		public Project Project { get; set; }
+	}
+}
