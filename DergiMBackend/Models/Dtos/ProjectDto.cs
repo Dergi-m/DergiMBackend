80f69bd3814cc5318d3db8380cad1f30fbc69be2
OlexanderPylypchuk <sashylichka2016@gmail.com>
2025-04-14 22:22:00 +0300

    ProjectDtoFix

--- DergiMBackend/Models/Dtos/ProjectDto.cs
+++ DergiMBackend/Models/Dtos/ProjectDto.cs
@@ -5,8 +5,8 @@ namespace DergiMBackend.Models.Dtos
 	public class ProjectDto
 	{
 		public int Id { get; set; }
-		public string Name { get; set; }
-		public string Description { get; set; }
+		public string? Name { get; set; }
+		public string? Description { get; set; }
 		public int OrganisationId { get; set; }
 		public IEnumerable<ProjectFileDto>? ProjectFiles { get; set; }
 	}
