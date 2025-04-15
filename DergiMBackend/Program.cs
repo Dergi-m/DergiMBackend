08130a4f1a449854f5abb51d3f7bc7f5d7a08db8
OlexanderPylypchuk <sashylichka2016@gmail.com>
2025-04-15 15:01:39 +0300

    Migrations

--- DergiMBackend/Program.cs
+++ DergiMBackend/Program.cs
@@ -93,5 +93,18 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-
+ApplyMigrations();
 app.Run();
+
+void ApplyMigrations()
+{
+	using (var scope = app.Services.CreateScope())
+	{
+		var _db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+		if (_db != null && _db.Database.GetPendingMigrations().Count() > 0)
+		{
+			_db.Database.Migrate();
+		}
+	}
+}
